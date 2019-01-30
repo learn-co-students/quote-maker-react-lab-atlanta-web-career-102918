dcd8c5052eac44b6fb234527e899304fa378a03b
Andrew Jones <joneseyit@protonmail.com>
2019-01-30 10:25:44 -0500

    added onChange handler

--- src/containers/QuoteForm.js
+++ src/containers/QuoteForm.js
@@ -7,10 +7,16 @@ class QuoteForm extends Component {
 
   state = {
     //set up a controlled form with internal state
+    content: '',
+    author: ''
   }
 
   handleOnChange = event => {
     // Handle Updating Component State
+    this.setState({
+        [event.target.name]: event.target.value
+     })
+    console.log(this.state)
   }
 
   handleOnSubmit = event => {
@@ -32,8 +38,10 @@ class QuoteForm extends Component {
                     <label htmlFor="content" className="col-md-4 control-label">Quote</label>
                     <div className="col-md-5">
                       <textarea
+                        name="content"
                         className="form-control"
                         value={this.state.content}
+                        onChange={event => this.handleOnChange(event)}
                       />
                     </div>
                   </div>
@@ -41,9 +49,11 @@ class QuoteForm extends Component {
                     <label htmlFor="author" className="col-md-4 control-label">Author</label>
                     <div className="col-md-5">
                       <input
+                        name="author"
                         className="form-control"
                         type="text"
                         value={this.state.author}
+                        onChange={event => this.handleOnChange(event)}
                       />
                     </div>
                   </div>
@@ -62,5 +72,7 @@ class QuoteForm extends Component {
   }
 }
 
+//todo: add mapStatetoProps/MapdispatchtoProps
+
 //add arguments to connect as needed
-export default connect()(QuoteForm);
+export default connect(null, null)(QuoteForm);
