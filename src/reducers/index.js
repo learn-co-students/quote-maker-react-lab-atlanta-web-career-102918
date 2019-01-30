6bfdfabef4241ccf9e41115f594cb5f309145c57
Andrew Jones <joneseyit@protonmail.com>
2019-01-30 10:10:29 -0500

    got quote form and quotes to render

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 import quotes from './quotes';
 
+
+//TODO: export a root reducer
+//TODO: create quoteReducer & import into this file
 export default combineReducers({
   quotes
 });
