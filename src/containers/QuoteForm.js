13f98ac982e9872004d9bf973aa8662c5900ea9b
Andrew Jones <joneseyit@protonmail.com>
2019-01-30 10:49:41 -0500

    got handleOnSubmit to get hit

--- src/containers/QuoteForm.js
+++ src/containers/QuoteForm.js
@@ -20,7 +20,9 @@ class QuoteForm extends Component {
   }
 
   handleOnSubmit = event => {
-    // Handle Form Submit event default
+    event.preventDefault();
+    
+    // Handle Form Submit event def ault
     // Create quote object from state
     // Pass quote object to action creator
     // Update component state to return to default state
@@ -33,7 +35,7 @@ class QuoteForm extends Component {
           <div className="col-md-8 col-md-offset-2">
             <div className="panel panel-default">
               <div className="panel-body">
-                <form className="form-horizontal">
+                <form className="form-horizontal" onSubmit={event => this.handleOnSubmit(event)}>
                   <div className="form-group">
                     <label htmlFor="content" className="col-md-4 control-label">Quote</label>
                     <div className="col-md-5">
@@ -75,4 +77,4 @@ class QuoteForm extends Component {
 //todo: add mapStatetoProps/MapdispatchtoProps
 
 //add arguments to connect as needed
-export default connect(null, null)(QuoteForm);
+export default connect()(QuoteForm);
